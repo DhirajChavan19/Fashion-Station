7e7a2dfde93c43804b1faab1a9ec76e12c72e528
shindeprem <prembshinde1210@gmail.com>
2023-10-03 19:56:32 +0530

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
   return(
-    <></>
+    <Routes>
+      <Route path='/' element={}/>
+    </Routes>
   )
 }
 
