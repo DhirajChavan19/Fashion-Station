af2924343ebdd7c0da54cf627d2f8247afc69359
shindeprem <prembshinde1210@gmail.com>
2023-10-04 21:05:27 +0530

    Adding Footer

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/header";
+import Footer from "../../components/Footer/footer";
 
 const MainPage = () =>{
     return(
         <>
             <Header/>
             <Outlet/>
-        
+            <Footer/>
         </>
     )
 }
