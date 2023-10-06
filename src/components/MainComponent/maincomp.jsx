85ab0154d5fa86924e0677dc0d41de3700d97910
shindeprem <prembshinde1210@gmail.com>
2023-10-06 19:55:56 +0530

    fixed main component

--- src/components/MainComponent/maincomp.jsx
+++ src/components/MainComponent/maincomp.jsx
@@ -1,13 +1,14 @@
-import MainHeader from '../MainHeader/MainHeader';
-import TopHeader from '../TopHeader/TopHeader';
 import './maincomp.css'
 
 const MainComp = ()=>{
     return(
-        <>
+        <div className='mainPageImg'>
         {/* main page content will come on this page */}
         {/* which is imported in mainpagecomp */}
-        </>
+        <div className="img-content-container">
+
+        </div>
+        </div>
     )
 }
 
