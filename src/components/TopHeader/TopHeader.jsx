8839568d905ed6889bf916082515949e9723dcc5
shindeprem <prembshinde1210@gmail.com>
2023-10-06 12:23:51 +0530

    --fix

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import './TopHeader.css'
 import {RxCross2} from 'react-icons/rx'
 
 const TopHeader = ()=>{
+
+    const [isTopHeaderOpen,setIsTopHeaderOpen] = useState(true);
+
+    const closeTopHeader = () =>{
+        setIsTopHeaderOpen(false)
+    }
+
     return(
-        <div className="top-blck-header">
+        <div className={`top-blck-header ${isTopHeaderOpen?'openTopHeader':'closeTopHeader'}`}>
             <div className="top-header-nav">
                 <div className="top-header-content">
                     Free Shipping on all orders above $50
@@ -15,6 +22,9 @@ const TopHeader = ()=>{
                         <option value="signin">Sign In</option>
                         <option value="signup">Sign Up</option>
                     </select>
+                </div>
+                <div className="close-icon" onClick={closeTopHeader}>
+                    <RxCross2/>
                 </div>
             </div>
         </div>
