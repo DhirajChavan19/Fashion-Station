99180758977daa50bff2d79f0bcfbf2731b33300
shindeprem <prembshinde1210@gmail.com>
2023-10-06 12:08:06 +0530

    --feat Added top header

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import './TopHeader.css'
+import {RxCross2} from 'react-icons/rx'
 
 const TopHeader = ()=>{
     return(
-        <></>
+        <div className="top-blck-header">
+            <div className="top-header-nav">
+                <div className="top-header-content">
+                    Free Shipping on all orders above $50
+                </div>
+                <div className="top-header-acct">
+                    <select name="acct-selected" id="acct-set">
+                        <option value="select-opt">My Account</option>
+                        <option value="signin">Sign In</option>
+                        <option value="signup">Sign Up</option>
+                    </select>
+                </div>
+            </div>
+        </div>
     )
 }
 
