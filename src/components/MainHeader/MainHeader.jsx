61999f543d5a6d293df325b0725ee83fd6ca2889
shindeprem <prembshinde1210@gmail.com>
2023-10-06 15:53:46 +0530

    --feat Added main header

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import './MainHeader.css';
+import {BiSearch} from 'react-icons/bi'
+import {FaUser} from 'react-icons/fa'
+import {FaShoppingCart} from 'react-icons/fa'
 
 const MainHeader = () =>{
+
+    const [isSearchOpen,setIsSearchOpen] = useState(false)
+
+    const expandSearchInput = () =>{
+        setIsSearchOpen(true)
+    }
     return(
-        <></>
+        <div className="main-header-container">
+            <div className="main-header-nav">
+                <div className="web-logo">
+                    <div className="black-web-logo" style={{color:"black"}}>
+                        FASHION
+                    </div>
+                    <div className="orng-web-logo" style={{color:'#ed574e'}}>
+                        STATION
+                    </div>
+                </div>
+
+                <div className="header-content">
+                    <div className="categories-content-links">
+                        <div className="mens-section">Men</div>
+                        <div className="womens-section">Women</div>
+                        <div className="kids-section">
+                            Kids
+                        </div>
+                    </div>
+
+                    <div className="header-menu-containner">
+                        <div className="search-icon-input-container" onClick={expandSearchInput} >
+                            <input type="text" name="userSearch" className="user-search-input" style={isSearchOpen?{maxWidth:"140px"}:{width:"10px"}}/>
+                            <BiSearch className="search-icon"/>
+                        </div>
+                        <div className="user-icon-container">
+                            <FaUser/>
+                        </div>
+                        <div className="cart-icon-container">
+                            <FaShoppingCart/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
